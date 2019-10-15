bc99e21be484d37b39e8dc2a90f075f57a2207bb
Maegan Wilson <maegan@maeganwilson.com>
2019-10-15 07:03:13 -0500

    Adding watchos

---
+++ buttons-watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  buttons-watch WatchKit Extension
+//
+//  Created by Maegan Wilson on 10/15/19.
+//  Copyright © 2019 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView{
+            Button(action: {
+                print("Bordered Button")
+            }){
+                Text("Bordered Button: Not available")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ buttons-watch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  buttons-watch WatchKit Extension
+//
+//  Created by Maegan Wilson on 10/15/19.
+//  Copyright © 2019 Maegan Wilson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
